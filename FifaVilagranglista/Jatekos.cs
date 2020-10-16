caaa21acfc134eeec9e1dbdce97b2d05967934fe
Axmart <notaxmart@gmail.com>
2020-10-16 20:44:31 +0200

    New Jatekos class and fifa.txt source

---
+++ FifaVilagranglista/Jatekos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifaVilagranglista
+{
+    class Jatekos
+    {
+
+        public string Csapat { get; set; }
+        public int Helyezes { get; set; }
+        public int Valtozas { get; set; }
+        public int Pontszam { get; set; }
+
+
+
+    }
+}
--- FifaVilagranglista/Program.cs
+++ FifaVilagranglista/Program.cs
@@ -10,6 +10,9 @@ namespace FifaVilagranglista
     {
         static void Main(string[] args)
         {
+
+           
+
         }
     }
 }
