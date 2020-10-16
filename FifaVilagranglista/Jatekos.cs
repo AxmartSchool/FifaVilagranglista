9ec4afcfb93506bb0339a50d008252bdd7edb873
Axmart <notaxmart@gmail.com>
2020-10-16 20:50:46 +0200

    Beolvasas method

--- FifaVilagranglista/Jatekos.cs
+++ FifaVilagranglista/Jatekos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,24 @@ namespace FifaVilagranglista
         public int Pontszam { get; set; }
 
 
+        static List<Jatekos> Beolvasas()
+        {
+
+            var sr = new StreamReader("../../fifa.txt", Encoding.UTF8);
+            var outputList = new List<Jatekos>();
+            string[] tempTomb;
+
+            while (!sr.EndOfStream)
+            {
+                tempTomb = sr.ReadLine().Split(';');
+                outputList.Add(new Jatekos() { Csapat = tempTomb[0], Helyezes=int.Parse(tempTomb[1]), Valtozas= int.Parse(tempTomb[2]), Pontszam= int.Parse(tempTomb[3]) });
+
+            }
+
+            sr.Close();
+            return outputList;
+
+        }
 
     }
 }
